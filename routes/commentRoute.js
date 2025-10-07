90b5d9826093f5334a33757eece4e7f70b1d53ac
sawanyadav07 <sawany250@gmail.com>
2025-10-07 22:21:38 +0530

    added comment update api

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -5,8 +5,8 @@ const {auth}= require('../middlewares/verifyToken.js');
 const {createComment, getComment, updateComment, deleteComment }= require('../controllers/commentController.js');
 
 router.post('/create',auth, createComment);
-// router.get('/getById/:id',auth, getComment);
-// router.put('/update/:id',auth, updateComment);
+router.get('/getById/:id',auth, getComment);
+router.put('/update/:id',auth, updateComment);
 // router.delete('/delete/:id',auth, deleteComment);
 
 module.exports= router;
