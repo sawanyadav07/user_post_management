4fba890f442c99f85413a8f833244cb26cab013f
sawanyadav07 <sawany250@gmail.com>
2025-10-31 23:49:38 +0530

    fix bug in auth controller in register api

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ exports.register = async (req, res, next) => {
         const {name, userName, password }= req.body;
 
         const userExist= await User.findOne({ userName });
-          if(userExist) return next(new customError("User already exists", 401))
+          if(userExist) return
 
         const hashedPassword=await hashPassword(password);
         const userPlayload= {
@@ -20,14 +20,49 @@ exports.register = async (req, res, next) => {
         };
 
         const newUser= await User.create(userPlayload);
-        if (newUser) return  next(new customError("user register successfully", 200))           
-       else return next(new customError("Failed to register users", 401))
+        if (newUser) return           
+       else return
     } catch (error) {
         res.status(500).json({ message: "Internal server error" });
-        return next(new customError("Internal server error", 401))
-        
+        return 
     }
 }
+
+exports.register = async (req, res, next) => {
+  try {
+    const { name, userName, password } = req.body;
+
+     if (!name || !userName || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const userExist = await User.findOne({ userName });
+    if (userExist) return res.status(400).json({ message: "User already exists" });
+    
+    const hashedPassword = await hashPassword(password);
+
+    const userPayload = {
+      name,
+      userName,
+      password: hashedPassword,
+    };
+
+    const newUser = await User.create(userPayload);
+
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        userName: newUser.userName,
+      },
+    });
+  } catch (error) {
+    console.error("Register Error:", error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 exports.login =async (req, res, next) => {
     try {
